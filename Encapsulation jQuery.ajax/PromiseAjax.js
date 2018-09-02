3ac36cf9df0f4fac7e35452fb42af930538bbe62
MingJiang3 <457918455@qq.com>
2018-09-02 18:32:07 +0800

    Promise ajax

--- Encapsulation jQuery.ajax/PromiseAjax.js
+++ Encapsulation jQuery.ajax/PromiseAjax.js
@@ -1,19 +1,21 @@
 window.jQuery = function(nodeOrSelector) {let nodes = {};nodes.addClass = function(){};nodes.html =  function(){};return nodes} 
-window.jQuery.ajax = function({url,method,body,successFn,failFn,headers}) {
-    let request = new XMLHttpRequest()
-    request.open(method,url)
-    for(let key in headers){
-        let value = headers[key]
-        resquest.setRequestHeader(key,value)
-    }
-    request.onreadystatechange = function(){
-        if (request.readyState === 4) {
-            if (request.status >= 200 && request.status < 300) {
-                successFn.call(undefined,request.responseText)
-            }else if(request.status >= 400){
-                failFn.call(undefined,request)
+window.jQuery.ajax = function({url,method,body,headers}) {
+    return new Promise(function(resolve,reject){
+        let request = new XMLHttpRequest()
+        request.open(method,url)
+        for(let key in headers){
+            let value = headers[key]
+            resquest.setRequestHeader(key,value)
+        }
+        request.onreadystatechange = function(){
+            if (request.readyState === 4) {
+                if (request.status >= 200 && request.status < 300) {
+                    resolve.call(undefined,request.responseText)
+                }else if(request.status >= 400){
+                    reject.call(undefined,request)
+                }
             }
         }
-    }
-    request.send(body)
+        request.send(body)
+    })
 }
