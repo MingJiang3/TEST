ed01f877eb7124d107df3af192436174b667e0ab
MingJiang3 <457918455@qq.com>
2018-10-27 22:42:43 +0800

    ts 测试

---
+++ TypeScript-test1/min.ts
@@ -0,0 +1,6 @@
+function min(a:number,b:number):number {
+    return a>b ? b:a
+}
+
+var c = min(7,9)
+console.log(c)
--- TypeScript-test1/test.ts
+++ TypeScript-test1/test.ts
@@ -1,1 +1,22 @@
-console.log('hello qq')
+class Student {
+    fullName: string;
+    firstName: string;
+    middleInitial: string;
+    lastName: string;
+    constructor( firstName,  middleInitial,  lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+function greeter(person : Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+let user = new Student("Jane", "M.", "User");
+console.log(greeter(user))
+
